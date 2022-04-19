a3a3fb2060d2a13cae960a46c7b896a02ab06fe4
Jacrys3 <ryse0528@gmail.com>
2022-04-18 20:06:54 -0700

    Idk whats happening with .bat files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function Hello(props) {
-    return <h1>Hello World!</h1>;
+    return <h1 class="hi">Hello World!</h1>;
 }
 
 ReactDOM.render(<Hello />, document.getElementById("root"));
