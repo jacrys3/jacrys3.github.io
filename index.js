430e39fa724c8da0c4834b0de91a78313a20c098
Jacrys3 <ryse0528@gmail.com>
2022-04-18 21:11:57 -0700

    I think I got my js to show up

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
+function Hello() {
+  return <h1>Hello World!</h1>;
+}
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+ReactDOM.render(<Hello />, document.getElementById('mydiv'))
